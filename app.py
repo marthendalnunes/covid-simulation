17bbf464542346f5e5d251af2ae328a5fae5a0d4
Vitor Marthendal <vrn@neo.ufsc.br>
2020-06-14 11:26:09 -0300

    clean app

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
-import seir
-import about
+#import seir
+#import about
 
-PAGES = {
-    "Seir": seir,
-    "About": about
- }
+#PAGES = {
+#    "Seir": seir,
+#    "About": about
+# }
 
 def main():
     st.markdown("test")
     
     st.sidebar.markdown("# Navegação")
-    goto = st.sidebar.radio("Ir para", list(PAGES.keys()))
-    PAGES[goto].main()
+#    goto = st.sidebar.radio("Ir para", list(PAGES.keys()))
+#    PAGES[goto].main()
 
 if __name__=="__main__":
     main()
