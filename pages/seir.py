6d0827e0777663d5c7094f23de8179e2d1d77516
Vitor Marthendal <vrn@neo.ufsc.br>
2020-06-14 12:52:25 -0300

    add run button

--- pages/seir.py
+++ pages/seir.py
@@ -6,5 +6,8 @@ def main():
     st.markdown("# Seir Model")
     st.sidebar.number_input("R0", R0)
 
+    if st.sidebar.button("Rodar simulação"):
+        st.write("Rodando")
+
 if __name__=="__main__":
     main()
