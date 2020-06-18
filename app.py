08be62f26ec09d3be5972ce929cb5bb8b49f01ef
vitor_marthendal <vrn@neo.ufsc.br>
2020-06-17 23:57:23 -0300

    add cadcad SEIR model

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import streamlit as st
+import run
 
-import pages.seir
-import pages.about
-
-PAGES = {
-    "Seir": pages.seir,
-    "About": pages.about
-}
+R0 = 2.5
 
 def main():
-    st.markdown("# Covid-19 model simulator")
-    
-    st.sidebar.markdown("# Navegação")
-    goto = st.sidebar.radio("Ir para", list(PAGES.keys()))
-    PAGES[goto].main()
+    st.markdown("# Seir Model")
+    st.sidebar.number_input("R0", R0)
+   
+    st.sidebar.button("Rodar simulação")
+    experiments = run.run()
+    seir_df = experiments.dataset[0]
+    st.write(seir_df)
+    seir_df = seir_df.filter(items=['susceptible','exposed','infected','recovered','dead'])
+    st.line_chart(seir_df)
+	
 
 if __name__=="__main__":
     main()
